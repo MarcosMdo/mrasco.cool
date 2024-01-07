b2a2f201a6e3eb0388d44f86ba9118323e6f5eda
Marcos Montes de Oca <26945566+MarcosMdo@users.noreply.github.com>
2024-01-07 01:39:50 -0800

    adds analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 'use client'
 import styles from './page.module.css'
 import Toaster from './components/toaster/Toaster';
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Toaster></Toaster>
+      <Analytics />
     </main>
   )
 }
